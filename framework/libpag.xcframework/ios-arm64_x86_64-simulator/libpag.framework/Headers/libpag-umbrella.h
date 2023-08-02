2f5fd7ac04ddc53325dafe80e148babb17c9b8dd
libpag <libpag@tencent.com>
2023-08-02 15:07:13 +0800

    Push From Landun: update SDK

--- framework/libpag.xcframework/ios-arm64_x86_64-simulator/libpag.framework/Headers/libpag-umbrella.h
+++ framework/libpag.xcframework/ios-arm64_x86_64-simulator/libpag.framework/Headers/libpag-umbrella.h
@@ -38,7 +38,6 @@
 #import "PAGLicenseManager.h"
 #import "PAGMovie.h"
 #import "PAGMovieExporter.h"
-#import "PAGView+Audio.h"
 
 FOUNDATION_EXPORT double libpagVersionNumber;
 FOUNDATION_EXPORT const unsigned char libpagVersionString[];
